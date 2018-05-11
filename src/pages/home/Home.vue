3cb06094d80ab4d4e084cf8ca0dfb1acf1cea978
hanli <hkl450513@163.com>
2018-05-11 13:29:56 +0800

    style config

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components:{
+    HomeHeader:HomeHeader
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont">&#xe624;</div>
+    </div>
+    <div class="header-input" >
+      <span class="iconfont">&#xe632;</span>
+      请输入城市/景点/游玩名称
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+  @import '~styles/base.scss';
+
+  .header {
+    display: flex;
+    font-size:14px;
+    line-height: 2.1rem;
+    color:#fff;
+    background:$bgcolor;
+    .header-left{
+      width:1.61rem;
+      div{
+        text-align: center;
+        font-size:1rem
+      }
+    }
+    .header-input{
+      flex:1;
+      padding-left: .5rem;
+      margin-top: .3rem;
+      margin-left:.5rem;
+      border-radius:.2rem;
+      background:#fff;
+      color:#ccc;
+      height: 1.5rem;
+      line-height: 1.5rem;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .header-right{
+      // width:1.8rem;
+      margin: 0 .5rem;
+      text-align: center;
+      .iconfont{
+        font-size: .5rem
+      }
+    }
+  }
+    
+</style>
